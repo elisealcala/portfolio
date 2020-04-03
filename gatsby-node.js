3da67043632c55f449b972e729fb63e663db30d3
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-02 22:03:07 -0500

    create dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,56 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { createFilePath } = require('gatsby-source-filesystem')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.tsx`)
+  return graphql(
+    `
+      query loadPagesQuery($limit: Int!) {
+        allMdx(limit: $limit) {
+          edges {
+            node {
+              id
+              body
+              frontmatter {
+                slug
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `,
+    { limit: 1000 }
+  ).then((result) => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog post pages.
+    result.data.allMdx.edges.forEach((edge) => {
+      createPage({
+        // Path for this page — required
+        path: `${edge.node.frontmatter.slug}`,
+        component: blogPostTemplate,
+        context: {
+          slug: `${edge.node.fields.slug}`,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
