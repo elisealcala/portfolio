4cf5a510b9409a86fd6f616b33c3d7e9a15b59f2
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-10 17:05:20 -0500

    update gatbsy-ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from 'react';
+import 'normalize.css/normalize.css';
+import './src/css/styles.css';
+import theme from 'prism-react-renderer/themes/nightOwl';
+import { MDXProvider } from '@mdx-js/react';
+import Code from './src/components/code';
 
-// You can delete this file if you're not using it
+const components = {
+  pre: (props) => {
+    const className = props.children.props.className || '';
+    const matches = className.match(/language-(?<lang>.*)/);
+    return (
+      <Code
+        codeString={props.children.props.children.trim()}
+        theme={theme}
+        lang={matches?.groups?.lang ? matches.groups.lang : ''}
+      />
+    );
+  },
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>;
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,63 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import 'normalize.css/normalize.css';
+import './src/css/styles.css';
+import theme from 'prism-react-renderer/themes/nightOwl';
+import { MDXProvider } from '@mdx-js/react';
+import Code from './src/components/code';
 
-// You can delete this file if you're not using it
+const components = {
+  pre: (props) => {
+    const className = props.children.props.className || '';
+    const matches = className.match(/language-(?<lang>.*)/);
+    return (
+      <Code
+        codeString={props.children.props.children.trim()}
+        theme={theme}
+        lang={matches?.groups?.lang ? matches.groups.lang : ''}
+      />
+    );
+  },
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>;
+};
+
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    React.createElement('script', {
+      dangerouslySetInnerHTML: {
+        __html: `
+          (() => {    
+            window.__onThemeChange = function() {};                
+            function setTheme(newTheme) {                  
+              window.__theme = newTheme;                  
+              preferredTheme = newTheme;                  
+              document.body.className = newTheme;                 
+              window.__onThemeChange(newTheme);                
+            }
+
+            let preferredTheme
+            try {
+              preferredTheme = localStorage.getItem('theme')
+            } catch (err) {}
+
+            window.__setPreferredTheme = newTheme => {
+              setTheme(newTheme)
+              try {
+                localStorage.setItem('theme', newTheme)
+              } catch (err) {}
+            }
+
+            let darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+            darkQuery.addListener(e => {
+              window.__setPreferredTheme(e.matches ? 'light' : 'dark')
+            })
+
+            setTheme(preferredTheme || (darkQuery.matches ? 'light' : 'dark'))
+          })()
+        `,
+      },
+    }),
+  ]);
+};
