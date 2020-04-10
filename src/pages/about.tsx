f01a708e5220ab4e07be9fdb0169926da5345f09
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-10 17:05:49 -0500

    create pages for about and blog

---
+++ src/pages/about.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 300;
+    margin: 0 0 16px 0;
+    span {
+      font-weight: 600;
+    }
+    @media (min-width: 768px) {
+      width: 450px;
+      font-size: 20px;
+      line-height: 32px;
+    }
+  }
+  @media (min-width: 768px) {
+    & .contact {
+      p {
+        font-size: 16px;
+      }
+    }
+  }
+`;
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <SEO title="About" />
+      <Wrapper>
+        <p>
+          Hi, I'm <span>Elizabeth</span>. I'm from Lima, Perú.
+        </p>
+        <p>
+          I've been working as a front-end developer for almost two years with
+          technologies such as <span>React</span>, <span>Typescript</span>,{' '}
+          <span>Apollo</span> and many more.
+        </p>
+        <p>Feel fre to contact me.</p>
+        <div className="contact">
+          <p>
+            <a target="_blank" href="https://twitter.com/elisealcala">
+              <span>Twitter</span>
+            </a>
+          </p>
+          <p>
+            <a target="_blank" href="https://github.com/elisealcala">
+              <span>Github</span>
+            </a>
+          </p>
+          <p>
+            <a target="_blank" href="https://www.linkedin.com/in/elizalcala/">
+              <span>Linkedin</span>
+            </a>
+          </p>
+          <p>
+            <span>elizabethalcala28@gmail.com</span>
+          </p>
+        </div>
+      </Wrapper>
+    </Layout>
+  );
+}
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import styled from 'styled-components';
-import { graphql, Link } from 'gatsby';
 import SEO from '../components/seo';
 import BlogCard from '../components/blog-card';
 import Layout from '../components/layout';
@@ -9,28 +9,13 @@ const Wrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  & .description {
-    font-size: 24px;
-    font-weight: 300;
-    margin: 0 0 16px 0;
-    @media (min-width: 768px) {
-      width: 450px;
-      font-size: 32px;
-    }
-  }
-  .blog_link {
-    margin-top: 30px;
-    font-size: 14px;
-    text-decoration: underline;
-  }
 `;
 
 export const postQuery = graphql`
-  query postQuery {
+  query blogQuery {
     allMdx(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { published: { eq: true } } }
-      limit: 4
     ) {
       edges {
         node {
@@ -50,21 +35,14 @@ export const postQuery = graphql`
   }
 `;
 
-export default function IndexPage({ data }) {
+export default function Blog({ data }) {
   return (
     <Layout>
       <Wrapper>
         <SEO title="Home" />
-        <h1 className="description">
-          {`Hi, \n my name is \n Elizabeth Alcalá.\n I'm a frontend developer.`}
-        </h1>
-        <p>Check my latest posts.</p>
         {data.allMdx.edges.map((mdx) => (
           <BlogCard key={mdx.node.id} {...mdx.node} />
         ))}
-        <div className="blog_link">
-          <Link to="/blog">View all</Link>
-        </div>
       </Wrapper>
     </Layout>
   );
