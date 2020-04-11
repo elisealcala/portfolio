8d9562207aecefc6ab62b7a7a3dea7d485fd8770
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-10 19:44:28 -0500

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `elisealcala`,
     description: `Frontend Developer`,
-    author: `@gatsbyjs`,
+    author: `@elisealcala`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -43,8 +43,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000`,
+        theme_color: `#000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -53,4 +53,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
