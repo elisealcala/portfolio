e1827fbe7fc1885386275452f75a73a13b1f9806
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-04-10 20:10:50 -0500

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
         background_color: `#000`,
         theme_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-dark.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
