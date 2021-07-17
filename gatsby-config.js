2afab276e40067eda5937252596f5be46c998f03
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2021-07-17 12:57:50 -0500

    feat: update gatsby version

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         icon: `src/images/favicon-dark.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify-cms`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
